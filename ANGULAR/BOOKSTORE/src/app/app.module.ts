b60770ec784be695e222fc5f8a0b42e7114c0b9e
kirthivashan <vkvashan24@gmail.com>
2024-07-31 18:44:22 +0530

    ANGULAR Tasks

--- ANGULAR/BOOKSTORE/src/app/app.module.ts
+++ ANGULAR/BOOKSTORE/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
